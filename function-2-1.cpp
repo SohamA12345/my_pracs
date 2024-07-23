adf4c8e8365c7d9c5c59f39539808fe2532d352c
Soham <sohamaich12345@gmail.com>
2024-07-23 18:46:03 +0930

    Finished question 2-1

---
+++ function-2-1.cpp
@@ -0,0 +1,13 @@
+int min_element(int array[], int n) {
+  int min = 0;
+
+  if (n >= 1) {
+    min = array[0];
+  }
+
+  for (int i = 1; i < n; i++) {
+    if (min > array[i]) {
+      min = array[i];
+    }
+  }
+}
