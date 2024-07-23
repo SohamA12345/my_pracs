20f7aa830ec6a73add7292ce71e8136f7f022e25
Soham <sohamaich12345@gmail.com>
2024-07-23 19:39:43 +0930

    Finished question 2-4

--- function-2-4.cpp
+++ function-2-4.cpp
@@ -1,7 +1,10 @@
 bool is_ascending(int array[], int n) {
+  if (n < 1) {
+    return false;
+  }
+
   for (int i = 0; i < n - 1; i++) {
-    if (n > 0 && array[i] < array[i+1]) {;} 
-    else {return false;}
+    if (array[i] < array[i+1]) {;} else {return false;}
   }
 
   return true;
--- main-2-4.cpp
+++ main-2-4.cpp
@@ -5,9 +5,13 @@ extern bool is_ascending(int array[], int n);
 int main(void) {
   int array1[6] = {1, 2, 3, 4, 5, 6};
 
-  std::cout << is_ascending(array1, 6) << std::endl;
+  std::cout << std::boolalpha << is_ascending(array1, 6) << std::endl; // Expecting true
 
   int array2[0];
 
-  std::cout << is_ascending(array2, 0) << std::endl;
+  std::cout << is_ascending(array2, 0) << std::endl; // Expecting false
+
+  int array3[5] = {100, 2, 50, 50, 60};
+
+  std::cout << is_ascending(array3, 5) << std::endl; // Expecting false
 }
