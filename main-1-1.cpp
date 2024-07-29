86c5a4476a97b3028b014bb4e8ecce0d11e8b15e
Soham <sohamaich12345@gmail.com>
2024-07-29 22:34:41 +0930

    Finished question 1-1

---
+++ main-1-1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+extern int sum_diagonal(int array[4][4]);
+
+int main() {
+  int array[4][4] = {{1, 2, 3, 4}, {1, 2, 3, 4}, {1, 2, 3, 4}, {1, 2, 3, 4}};
+  int sum = sum_diagonal(array);
+
+  std::cout << sum << std::endl; // expecting sum as 1+2+3+4 = 10.
+}
