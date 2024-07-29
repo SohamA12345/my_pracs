016b376508504ffeaa3e244f7f7825ae25cd247e
Soham <sohamaich12345@gmail.com>
2024-07-29 22:53:41 +0930

    Finished question 1-2

--- function-1-2.cpp
+++ function-1-2.cpp
@@ -1,5 +1,15 @@
 int is_identity(int array[10][10]) {
   for(int i = 0; i < 10; i++) {
+    if (!(array[i][i] == 1)) {
+      return 0;
+    } else {break;}
     
+    for (int j = 0; j < 10; j++) {
+      if (!(array[i][j] == 0)) {
+        return 0;
+      }
+    }
   }
+
+  return 1;
 }
---
+++ main-1-2.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+extern int is_identity(int array[10][10]);
+
+int main() {
+  int array[10][10] = {{1, 0, 0, 0, 0, 0, 0, 0, 0, 0}, {0, 1, 0, 0, 0, 0, 0, 0, 0, 0}, {0, 0, 1, 0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 1, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 1, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 1, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 1, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 0, 1, 0}, {0, 0, 0, 0, 0, 0, 0, 0, 0, 1}};
+  bool identity = is_identity(array);
+
+  std::cout << std::boolalpha << identity << std::endl; // expecting true.
+}
