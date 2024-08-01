5034c40128f4f7894acfd7ba0325f728f948287a
Soham <sohamaich12345@gmail.com>
2024-08-01 15:51:41 +0930

    Finished question 2-3

---
+++ function-2-3.cpp
@@ -0,0 +1,29 @@
+bool is_palindrome(int integers[], int length) {
+  for (int i = 0; i < length/2; i++) {
+    if (integers[i] != integers[length - 1 - i]) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+int sum_if_palindrome(int integers[], int length) {
+  int sum = 0;
+
+  for (int i = 0; i < length; i++) {
+    sum += integers[i];
+  }
+
+  return sum;
+}
+
+int sum_array_element(int integers[], int length) {
+  int sum = 0;
+
+  if(is_palindrome(integers, length)) {
+    sum = sum_if_palindrome(integers, length);
+  }
+
+  return sum;
+}
---
+++ main-2-3.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+extern int sum_array_element(int integers[], int length);
+
+int main() {
+  int array[5] = {1, 2, 55, 5, 1};
+  int sum = sum_array_element(array, 5);
+
+  std::cout << sum << std::endl; // expecting sum as 1+2+3+4 = 10.
+
+  return 0;
+}
