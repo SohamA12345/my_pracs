9446c251671c9c65d05206faf16f38d4104205d6
Soham <sohamaich12345@gmail.com>
2024-08-01 15:09:51 +0930

    Fixed question 1-2

--- function-1-2.cpp
+++ function-1-2.cpp
@@ -1,12 +1,16 @@
 int is_identity(int array[10][10]) {
   for(int i = 0; i < 10; i++) {
-    if (!(array[i][i] == 1)) {
-      return 0;
-    } else {break;}
-    
     for (int j = 0; j < 10; j++) {
-      if (!(array[i][j] == 0)) {
-        return 0;
+      if (i == j) {
+        if (!(array[i][j] == 1)) {
+          return 0;
+        } 
+      }
+
+      if (i != j) {
+        if (!(array[i][j] == 0)) {
+          return 0;
+        }
       }
     }
   }
