c6186e560b1bf2246b8a1398870986b68bb798a3
Soham <sohamaich12345@gmail.com>
2024-08-22 16:10:20 +0930

    Finished

--- main-1-2.cpp
+++ main-1-2.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 int main() {
   Asset a;
-  Asset b(5, "Good Asset");
+  Asset b(0, "Good Asset");
 
   cout << a.get_value() << " " << a.get_product_type() << endl;
   cout << b.get_value() << " " << b.get_product_type() << endl;
