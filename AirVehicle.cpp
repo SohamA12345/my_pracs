17eb7076023dd999b919eeeb9d4311afe67b3dbd
Soham <sohamaich12345@gmail.com>
2024-09-05 15:22:21 +0930

    asdf

--- AirVehicle.cpp
+++ AirVehicle.cpp
@@ -6,13 +6,13 @@ AirVehicle::~AirVehicle() {}
 
 AirVehicle::AirVehicle(int w) : weight(w) {}
 
-void AirVehicle::refuel() {}
-void fly(int headwind, int minutes);
+void AirVehicle::refuel() {this->fuel = 100;}
+void AirVehicle::fly(int headwind, int minutes) {numberOfFlights++;}
 
-int AirVehicle::get_weight() {}
-float AirVehicle::get_fuel() {}
-int AirVehicle::get_numberOfFlights() {}
+int AirVehicle::get_weight() {return this->weight;}
+float AirVehicle::get_fuel() {return this->fuel;}
+int AirVehicle::get_numberOfFlights() {return this->numberOfFlights;}
 
-int AirVehicle::set_weight(int weight) {}
-float AirVehicle::set_fuel(int fuel) {}
-int AirVehicle::set_numberOfFlights(int numberOfFlights) {}
+int AirVehicle::set_weight(int weight) {this->weight = weight;}
+float AirVehicle::set_fuel(int fuel) {this->fuel = fuel;}
+int AirVehicle::set_numberOfFlights(int numberOfFlights) {this->numberOfFlights = numberOfFlights;}
